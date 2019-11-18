c7bce6c471594387320e309bc03ccfb1ed81796a
empyriumz <xindai90@gmail.com>
2019-11-17 22:36:41 -0500

    optimized measurement and svd

--- circuit_dynamics.py
+++ circuit_dynamics.py
@@ -1,5 +1,6 @@
 import numpy as np 
 from scipy import sparse
+from scipy.sparse.linalg import eigsh
 from scipy.stats import unitary_group
 import random
 from timeit import default_timer as timer 
@@ -112,7 +113,7 @@ def measure(wave,prob):
 
 			# projection of wavefunction into z-down state
 			pdown1=down.dot(wave)
-			pdown=wave.conjugate().T.dot(pdown1)
+			# pdown=wave.conjugate().T.dot(pdown1)
 			pdown=1-pup
 
 			# probility of the measurement is determined by the projection 
@@ -125,11 +126,112 @@ def measure(wave,prob):
 				wave=(1/np.sqrt(pdown))*pdown1
 	return wave
 
+# projective single-site measurement in z-basis
+def measure_test(wave, prob, pos):
+    # there are two possible outcomes
+    choice = [0, 1]
+    op = np.random.choice(choice, 1, p=[1-prob, prob]) #determine if to measure on this site
+    # if the measurement is chosen at the given position
+    if op[0] == 1:
+        # construct \sigma_z_i in the many-body basis
+        temp = np.ones(2**(L-pos-1))
+        pz = np.concatenate((temp,-temp))
+        # repeat the pattern for 2**pos times
+        pz = sparse.diags(np.tile(pz, 2**pos))
+        '''
+        alternatively, this can be done with the more conventional method
+        pz = sparse.kron(sparse.identity(2**pos), sz, format="csr")
+        pz = sparse.kron(pz,sparse.identity(2**(L-pos-1)), format="csr")
+        but the kronecker product is slow in scipy
+        '''
+        #up = 0.5*(spid + pz)       
+           
+        # projection operator for spin down
+        #down = 0.5*(spid - pz) 
+        
+
+        # projection of wavefunction
+        '''
+        the spin-up/dowm projection operator is given by $1/2(I \pm \sigma_z)$
+        we compute \sigma_z dot \psi part only since the identity part is trivial.
+        This is also for avoiding the plus operation between 
+        two large sparse matrices which is slow numerically
+        '''
+        temp = pz.dot(wave)
+        '''
+        instead we only need to compute the plus and minus between vectors
+        '''
+        pup1 = 0.5*(wave + temp)
+        pdown1 = 0.5*(wave - temp)
+        # expectation values
+        temp = (wave.conjugate().T).dot(temp)
+        pup = 0.5 + 0.5*np.asscalar(temp.real)
+        pdown = 1 - pup
+
+        # projection of wavefunction into z-down state
+        #pdown1=down.dot(wave)
+        #pdown=wave.conjugate().T.dot(pdown1)
+        #pdown=np.asscalar(pdown.real)
+        
+        '''
+        in case the wavefunction is close to product state, the measurement 
+        might yield pup=1 or pdown=1. 
+        To avoid possible numerical errors where pup>1, we manually set the probability 
+        to be 1 or 0.
+        '''
+        if abs(pup-1)<1e-8: 
+            pup = 1.0
+            pdown = 0.0
+            wave = pup1
+        elif abs(pup)<1e-8:
+            pup = 0.0
+            pdown = 1.0
+            wave = pdown1
+        else:
+            pdown = 1 - pup
+            '''
+            probility of the measurement outcome is determined 
+            by the expetation value of projection operator
+            '''
+            out = np.random.choice([0,1],1,p=[pup, pdown])
+
+            # if the measurement projects the spin onto the z-up state
+            if out[0] == 0:
+                wave = (1/np.sqrt(pup))*pup1 # normalization of wavefunction
+            else:
+                wave = (1/np.sqrt(pdown))*pdown1
+            # print(pup, pdown)
+            
+    return wave
+    
+def ent2(wave, n, la): # von-Neumann and Renyi entanglement entropy
+	l = L
+	lb = l - la
+    # convert the wavefunction into a matrix for SVD
+	temp = np.reshape(wave,(int(2**la),int(2**lb)))
+	temp = sparse.csr_matrix(np.dot(temp, temp.conj().T))
+	sp = np.array(eigsh(temp, k=8, which='LM',return_eigenvectors=False))
+	# (*The singular values can be obtained from Sqrt[Eigenvalues[ConjugateTranspose[m].m]]. *)
+	# sp = sp[sp != 0]
+
+	# print(sp)
+	# chop small singular values to zero to avoid numerical instability
+	tol = 1e-10
+	sp[abs(sp) < tol] = 0.0
+	# retain only non-zero values to avoid feeding to log function
+	el = sp[sp != 0]
+	# el = sp**2
+	# EE in log2 base
+	return -np.dot(el,np.log2(el)), (1/(1-n))*np.log2(np.sum(sp**(n)))
+
+
 # time evolution consists of random unitaries + projective measurement
 def evo(steps, wave, prob):
 	# initiate empty lists for keeping data
 	von=np.zeros(steps, dtype='float64') # von-Neumann entropy
 	renyi=np.zeros(steps, dtype='float64') # Renyi entropy
+	von2 = np.zeros(steps, dtype='float64') # von-Neumann entropy
+	renyi2 = np.zeros(steps, dtype='float64') # Renyi entropy
 	neg=np.zeros(steps, dtype='float64') # logarithmic negativity
 	mut=np.zeros(steps, dtype='float64') # mutual information using von-Neumann entropy
 	mutr=np.zeros(steps, dtype='float64') # mutual information in terms of Renyi entropy
@@ -172,17 +274,25 @@ def evo(steps, wave, prob):
 		result=ent(wave,2,L//2)
 		von[t]=result[0]
 		renyi[t]=result[1]
+
+		result=ent2(wave,2,L//2)
+		von2[t]=result[0]
+		renyi2[t]=result[1]
 		# calculate logarithmic negativity and mutual information with tri-partition
-		result=logneg(wave,2,la,lb,lc1,lc2)
-		neg[t]=result[0]
-		mut[t]=result[1]
-		mutr[t]=result[2]
+		# result=logneg(wave,2,la,lb,lc1,lc2)
+		# neg[t]=result[0]
+		# mut[t]=result[1]
+		# mutr[t]=result[2]
 		
-	return von, renyi, neg, mut, mutr
+	# return von, renyi, neg, mut, mutr, wave
+	return von, renyi, von2, renyi2
+
 
 
 result = evo(time, psi, pro)
-np.savez_compressed('evolution_L=%s_t=%s_p=%s'%(L, time, pro), von=result[0], renyi=result[1],neg=result[2], mutual=result[3], mutualrenyi=result[4])
+print(result[0]-result[2])
+print(result[1]-result[3])
+#np.savez_compressed('evolution_L=%s_t=%s_p=%s'%(L, time, pro), von=result[0], renyi=result[1],neg=result[2], mutual=result[3], mutualrenyi=result[4])
 
 
 end = timer()
